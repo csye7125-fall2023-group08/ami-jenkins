d81cc7557d7207bbfdfe42c464c9d6aab2fb863a
MoukthikaM <manapati.m@northeastern.edu>
2023-10-13 15:37:48 -0400

    made changes to authentication

--- packer/jenkins/webapp.groovy
+++ packer/jenkins/webapp.groovy
@@ -2,14 +2,12 @@ multibranchPipelineJob('webapp') {
     displayName('webapp_pipeline')
     branchSources {
         git {
-            id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('git@github.com:cyse7125-fall2023-group08/webapp.git')
-            credentialsId('ssh-github')
-
+            id('123456789-webapp') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/cyse7125-fall2023-group08/webapp.git')
+            credentialsId('token-github')
+            
         }
     }
-   
-    
 }
-    
+
 
