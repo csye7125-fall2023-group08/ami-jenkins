91153560456c10cabe7cf875f3012ed25ad9bf42
Moukthika Manapati <99062202+MoukthikaM@users.noreply.github.com>
2023-10-13 15:40:29 -0400

    Assign 03 (#20)
    * added jenkins installation with config and files
    
    * added secrets
    
    * changed files
    
    * push changes for build
    
    * replace secrets
    
    * replace secrets
    
    * replace secrets
    
    * git ignore
    
    * made changes to authentication
    
    ---------
    
    Co-authored-by: MoukthikaM

--- packer/jenkins/webapp-db.groovy
+++ packer/jenkins/webapp-db.groovy
@@ -2,8 +2,8 @@ multibranchPipelineJob('webapp-DB') {
     displayName('webapp_DB_pipeline')
     branchSources {
         git {
-            id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('git@github.com:cyse7125-fall2023-group08/webapp-db.git')
+            id('123456789-db') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/cyse7125-fall2023-group08/webapp-db.git')
             credentialsId('ssh-github')
 
         }
