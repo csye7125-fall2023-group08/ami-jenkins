b03e5f63cae4a99a0e9cdb72de8d257a8545b2c2
Moukthika Manapati <99062202+MoukthikaM@users.noreply.github.com>
2023-10-21 15:18:46 -0400

    Web hook (#27)
    * Create main.yml
    
    * Update main.yml
    
    * webhook
    
    * webhook

--- packer/jenkins/webapp-db.groovy
+++ packer/jenkins/webapp-db.groovy
@@ -5,7 +5,12 @@ multibranchPipelineJob('webapp-DB') {
             id('123456789-db') // IMPORTANT: use a constant and unique identifier
             remote('https://github.com/cyse7125-fall2023-group08/webapp-db.git')
             credentialsId('token-github')
-
+            configure { node ->
+                def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+                spec('')
+                token("webapp-db")
+            }
+        }
         }
     }
    
