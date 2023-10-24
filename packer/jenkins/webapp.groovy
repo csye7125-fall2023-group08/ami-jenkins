aa7ebc509734dcff52b5ea0f6ebee1568d93ba81
Rebecca Biju <biju.r@northeastern.edu>
2023-10-23 23:15:58 -0400

    feat(helm-pipeline): Add pipeline for Helm Chart

--- packer/jenkins/webapp.groovy
+++ packer/jenkins/webapp.groovy
@@ -3,7 +3,7 @@ multibranchPipelineJob('webapp') {
     branchSources {
         git {
             id('123456789-webapp') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/cyse7125-fall2023-group08/webapp.git')
+            remote('https://github.com/csye7125-fall2023-group08/webapp.git')
             credentialsId('token-github')
             configure { node ->
                 def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
