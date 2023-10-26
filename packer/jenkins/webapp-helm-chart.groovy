5cd9e482d6706523d3012b0a0c2f76651b7dd8df
Rebecca Biju <biju.r@northeastern.edu>
2023-10-26 17:36:50 -0400

    feat(tag): Add tag configuration in webapp-helm-chart.groovy

--- packer/jenkins/webapp-helm-chart.groovy
+++ packer/jenkins/webapp-helm-chart.groovy
@@ -13,4 +13,18 @@ multibranchPipelineJob('webapp-helm-chart') {
             }
         }
     }
+    configure {
+        def traitBlock = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+        traitBlock << 'jenkins.plugins.git.traits.CloneOptionTrait' {
+            extension(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
+                shallow(false)
+                noTag(false)
+                reference()
+                depth(0)
+                honorRefspec(false)
+            }
+        }
+
+        traitBlock << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
+    }
 }
