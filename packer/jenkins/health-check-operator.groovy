c1c93de5390b6972133fde87370161a641e02105
Rebecca Biju <biju.r@northeastern.edu>
2023-11-23 10:37:32 -0500

    fix(operator): Add operator pipeline. Add plugins for reading JSON in Jenkinsfile for GOOGLE_CRENDENTIALS

---
+++ packer/jenkins/health-check-operator.groovy
@@ -0,0 +1,30 @@
+multibranchPipelineJob('health-check-operator') {
+    displayName('health_check_operator_pipeline')
+    branchSources {
+        git {
+            id('123456789-operator') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/csye7125-fall2023-group08/health-check-operator-test.git')
+            credentialsId('token-github')
+            configure { node ->
+                def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+                    spec('')
+                    token("health-check-operator-test")
+                }
+            }
+        }
+    }
+    configure {
+        def traitBlock = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+        traitBlock << 'jenkins.plugins.git.traits.CloneOptionTrait' {
+            extension(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
+                shallow(false)
+                noTag(false)
+                reference()
+                depth(0)
+                honorRefspec(false)
+            }
+        }
+
+        traitBlock << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
+    }
+}
